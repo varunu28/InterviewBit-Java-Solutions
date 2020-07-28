9a6008675adacea0176c7bb4b7ef777fab22a5b2
Rahul Sain <50570677+rahulsain3000@users.noreply.github.com>
2020-07-28 12:16:53 +0530

    Update Merge Intervals.java

--- Arrays/Problems/Merge Intervals.java
+++ Arrays/Problems/Merge Intervals.java
@@ -9,25 +9,38 @@
  */
 public class Solution {
     public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
-        ArrayList<Interval> result = new ArrayList<>();
-        for (int i = 0; i < intervals.size(); i++) {
-            Interval interval = intervals.get(i);
-            if (interval.end < newInterval.start) {
-                result.add(interval);
-            } else if (newInterval.end < interval.start) {
-                result.add(newInterval);
-                result.addAll(intervals.subList(i, intervals.size()));
-                return result;
-            } else {
-                int start = Math.min(newInterval.start, interval.start);
-                int end = Math.max(newInterval.end, interval.end);
-                newInterval.start = start;
-                newInterval.end = end;
-            }
+    if(intervals == null){
+        intervals = new ArrayList<Interval>();
+        intervals.add(newInterval);
+        return intervals;
+    }
+    else if(intervals.size() == 0){
+        intervals.add(newInterval);
+        return intervals;
+    }
+    Interval toInsert = newInterval;
+    int i = 0;
+    while(i<intervals.size()){
+        Interval current = intervals.get(i);
+        if(current.end<toInsert.start){
+            i++;
+            continue;
         }
-        
-        result.add(newInterval);
-        
-        return result;
+        else if(current.start>toInsert.end){
+            intervals.add(i,toInsert);
+            break;
+            }
+            else{
+                toInsert.start = Math.min(current.start, toInsert.start);
+                toInsert.end = Math.max(current.end, toInsert.end);
+                intervals.remove(i);
+            }
+            
+    }
+    if(i == intervals.size())
+    {
+        intervals.add(toInsert);
+    }
+    return intervals;
     }
 }
