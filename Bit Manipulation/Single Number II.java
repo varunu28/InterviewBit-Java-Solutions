05bef011ad2b07ca4ed1f7604ca75a0688f6693a
kc_terala <kcterala@gmail.com>
2022-08-22 22:39:42 +0530

    Single Number II solution modified

--- Bit Manipulation/Single Number II.java
+++ Bit Manipulation/Single Number II.java
@@ -2,12 +2,12 @@ public class Solution {
     // DO NOT MODIFY THE LIST. IT IS READ ONLY
     public static int singleNumber(final List<Integer> A) {
         int sum = 0;
-        for(int i=0;i<32;i++){
-            int bitSum =0;
-            for(int j=0;j<A.size();j++){
+        for(int i = 0; i < 32; i++){
+            int bitSum = 0;
+            for(int j = 0; j < A.size(); j++){
                 bitSum += ((A.get(j) >> i) & 1);
             }
-            sum += (bitSum%3) * Math.pow(2, i);
+            sum += (bitSum % 3) * Math.pow(2, i);
         }
         return sum;
     }
